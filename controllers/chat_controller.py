915240f87a285d990a0a946aeffae2fc6ece5cbf
Dmytro Grinevych <dmitriy.grinevich.qa@gmail.com>
2024-05-03 13:03:02 +0300

    change back to open ai controller

--- controllers/chat_controller.py
+++ controllers/chat_controller.py
@@ -15,21 +15,24 @@ client = OpenAI()
 chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
 
 
-# @chat_controller.route("/", methods=["GET", "POST"])
-# def index():
-#     if request.method == "POST":
-#         content = request.form["message"]
-#         chat_history.append({"role": "user", "content": content})
-#         assistant_response_content = OpenAIHandler.get_assistant_response(chat_history)
-#         chat_history.append({"role": "assistant", "content": assistant_response_content})
-#     return render_template("index.html", chat_history=chat_history)
-
-
 @chat_controller.route("/", methods=["GET", "POST"])
-def vertex():
-    prompt = request.args.get("prompt")
-    service = os.environ.get('K_SERVICE', 'Unknown service')
-    revision = os.environ.get('K_REVISION', 'Unknown revision')
-    response = vertex_ai.run(prompt)
+def index():
+    if request.method == "POST":
+        content = request.form["message"]
+        chat_history.append({"role": "user", "content": content})
+        assistant_response_content = OpenAIHandler.get_assistant_response(chat_history)
+        chat_history.append({"role": "assistant", "content": assistant_response_content})
+    return render_template("index.html", chat_history=chat_history)
 
-    return render_template('index.html', message=response.text, Service=service, Revision=revision)
+
+# @chat_controller.route("/", methods=["GET", "POST"])
+# def vertex():
+#     if request.method == "POST":
+#     content = request.form["message"]
+#     chat_history.append({"role": "user", "content": content})
+#     prompt = request.args.get("prompt")
+#     service = os.environ.get('K_SERVICE', 'Unknown service')
+#     revision = os.environ.get('K_REVISION', 'Unknown revision')
+#     response = vertex_ai.run(prompt)
+#
+#     return render_template('index.html', message=response.text, Service=service, Revision=revision)
