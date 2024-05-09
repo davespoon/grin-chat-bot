ad04c97ccd01f53d601fd3be321b9b559399224a
Dmytro Grinevych <dmitriy.grinevich.qa@gmail.com>
2024-05-09 11:36:48 +0300

    save working example

--- controllers/chat_controller.py
+++ controllers/chat_controller.py
@@ -1,7 +1,7 @@
 # import os
 
 from dotenv import load_dotenv
-from flask import Blueprint, render_template, request
+from flask import (render_template, request, Response, stream_with_context, jsonify, Blueprint)
 from openai import OpenAI
 
 from language_models.open_ai.OpenAIHandler import OpenAIHandler
@@ -14,11 +14,45 @@ client = OpenAI()
 chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
 
 
-@chat_controller.route("/", methods=["GET", "POST"])
+@chat_controller.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        content = request.form["message"]
-        chat_history.append({"role": "user", "content": content})
-        assistant_response_content = OpenAIHandler.get_assistant_response(chat_history)
-        chat_history.append({"role": "assistant", "content": assistant_response_content})
     return render_template("index.html", chat_history=chat_history)
+
+
+@chat_controller.route("/chat", methods=["POST"])
+def chat():
+    # content = request.form["message"]
+    # chat_history.append({"role": "user", "content": content})
+    # assistant_response_content = OpenAIHandler.get_assistant_response(chat_history)
+    # chat_history.append({"role": "assistant", "content": assistant_response_content})
+    # return render_template("index.html", chat_history=chat_history)
+
+    content = request.json["message"]
+    chat_history.append({"role": "user", "content": content})
+    return jsonify(success=True)
+
+
+@chat_controller.route("/stream", methods=["GET"])
+def stream():
+    def generate():
+        assistant_response_content = ""
+
+        with client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=chat_history,
+                stream=True,
+        ) as stream:
+            for chunk in stream:
+                if chunk.choices[0].delta and chunk.choices[0].delta.content:
+                    # Accumulate the content only if it's not None
+                    assistant_response_content += chunk.choices[0].delta.content
+                    yield f"data: {chunk.choices[0].delta.content}\n\n"
+                if chunk.choices[0].finish_reason == "stop":
+                    break  # Stop if the finish reason is 'stop'
+
+        # Once the loop is done, append the full message to chat_history
+        chat_history.append(
+            {"role": "assistant", "content": assistant_response_content}
+        )
+
+    return Response(stream_with_context(generate()), mimetype="text/event-stream")
